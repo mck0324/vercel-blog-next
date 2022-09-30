0a3f28e5d528a0f9a24d91a48e8359e6e99f4d9c
mck0324 <dch3030@naver.com>
2022-09-30 14:50:51 +0900

    feat:blog deploy

---
+++ pages/api/post/write.js
@@ -0,0 +1,20 @@
+import { createPost } from "../../../lib/posts"
+import { format } from 'date-fns'
+
+export default async function handler(req, res) {
+    const { id, title, content } = req.body
+
+    try {
+        // 에러발생
+        // throw new Error("dididididi")
+        await createPost({
+            id,
+            title,
+            date: format(new Date(), 'yyyy-MM-dd'),
+            content,
+        })
+        res.status(200).json({ message:'create success' })
+    } catch (error) {
+        res.status(500).json({ error:`create failed ${error}` })
+    }
+}
